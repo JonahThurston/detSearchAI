64b375c415a7ed1053499c41df6d738de8abdb23
JonahThurston <jonah.m.thurston@gmail.com>
2025-10-29 15:53:11 -0600

    Gather all results

---
+++ results_and_plots.py
@@ -0,0 +1,102 @@
+#iterative deepening search
+idsEasyNodes = 334.1
+idsEasyLength = 7.0
+idsMediumNodes = 28269.0
+idsMediumLength = 15.0
+idsHardNodes = 695113.1
+idsHardLength = 21.0
+idsWorstNodes = 134381277.0
+idsWorstLength = 31.0
+
+#uniform cost
+#tiles out of place heuristic
+uTopEasyNodes = 143.7
+uTopEasyLength = 7.0
+uTopMediumNodes = 6407.25
+uTopMediumLength = 15.0
+uTopHardNodes = 72320.5
+uTopHardLength = 21.0
+uTopWorstNodes = 181315.7
+uTopWorstLength = 30.05
+
+#tiles out of row and column heuristic
+uTorcEasyNodes = 143.7
+uTorcEasyLength = 7.0
+uTorcMediumNodes = 6407.25
+uTorcMediumLength = 15.0
+uTorcHardNodes = 72320.5
+uTorcHardLength = 21.0
+uTorcWorstNodes = 181315.7
+uTorcWorstLength = 30.05
+
+#manhattan distance heuristic
+uMdEasyNodes = 143.7
+uMdEasyLength = 7.0
+uMdMediumNodes = 6407.25
+uMdMediumLength = 15.0
+uMdHardNodes = 72320.5
+uMdHardLength = 21.0
+uMdWorstNodes = 181315.7
+uMdWorstLength = 30.05
+
+#greedy
+#tiles out of place heuristic
+gTopEasyNodes = 52.675
+gTopEasyLength = 10.7
+gTopMediumNodes = 624.8
+gTopMediumLength = 75.9
+gTopHardNodes = 716.2
+gTopHardLength = 94.55
+gTopWorstNodes = 771.55
+gTopWorstLength = 100.75
+
+#tiles out of row and column heuristic
+gTorcEasyNodes = 2035.4
+gTorcEasyLength = 59.25
+gTorcMediumNodes = 8066.777777777777
+gTorcMediumLength = 154.11111111111111
+gTorcHardNodes = 3187.5
+gTorcHardLength = 168.0
+gTorcWorstNodes = 12043.708333333334
+gTorcWorstLength = 156.66666666666666
+
+#manhattan distance heuristic
+gMdEasyNodes = 1797.175
+gMdEasyLength = 51.2
+gMdMediumNodes = 4150.8
+gMdMediumLength = 79.5
+gMdHardNodes = 4295.375
+gMdHardLength = 102.7
+gMdWorstNodes = 5444.55
+gMdWorstLength = 101.7
+
+#A-star
+#tiles out of place heuristic
+aTopEasyNodes = 13.35
+aTopEasyLength = 7.0
+aTopMediumNodes = 341.175
+aTopMediumLength = 15.0
+aTopHardNodes = 4887.875
+aTopHardLength = 21.0
+aTopWorstNodes = 100266.5
+aTopWorstLength = 30.05
+
+#tiles out of row and column heuristic
+aTorcEasyNodes = 35.925
+aTorcEasyLength = 7.0
+aTorcMediumNodes = 1285.675
+aTorcMediumLength = 15.0
+aTorcHardNodes = 15238.45
+aTorcHardLength = 21.0
+aTorcWorstNodes = 147064.0
+aTorcWorstLength = 30.05
+
+#manhattan distance heuristic
+aMdEasyNodes = 41.1
+aMdEasyLength = 7.0
+aMdMediumNodes = 1176.025
+aMdMediumLength = 15.0
+aMdHardNodes = 11163.95
+aMdHardLength = 21.0
+aMdWorstNodes = 116303.125
+aMdWorstLength = 30.05
